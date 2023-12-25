6a708cdcd08841942786c493af24494fa14ecd18
yogevgit10 <154273369+yogevgit10@users.noreply.github.com>
2023-12-25 22:51:58 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,8 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int x= Integer.parseInt(args[0]);
+		int y= Integer.parseInt(args[1]);
+		System.out.println(x + y);
 	}
 }
