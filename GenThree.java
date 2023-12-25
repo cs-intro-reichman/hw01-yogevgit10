a67d0de4a45a4cd0931296030a5b12edc99dd778
yogevgit10 <154273369+yogevgit10@users.noreply.github.com>
2023-12-25 22:54:29 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,18 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		
+		int x= Integer.parseInt(args[0]);
+		int y= Integer.parseInt(args[1]);
+		int random1 = (int) (Math.random() * (Math.abs(x-y)))+Math.min(x,y));
+		int random2 = (int) (Math.random() * (Math.abs(x-y)))+Math.min(x,y));
+		int random3 = (int) (Math.random() * (Math.abs(x-y)))+Math.min(x,y));
+		int minimum = Math.min(Math.min(random1,random2),random3);
+		System.out.println(random1);
+		System.out.println(random2);
+		System.out.println(random3);
+		System.out.println("The minimal generated number was " + minimum);
+
+
 	}
 }
